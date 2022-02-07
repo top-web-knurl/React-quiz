5616081d3d14865bbff2a5ccfc781c35496d06fc
Timur <58705107+top-web-knurl@users.noreply.github.com>
2022-02-07 22:35:07 +0300

    Кнопка меню, логика toggle, анимация и смена иконки при открытии

---
+++ src/components/Navigation/MenuToggle.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from './MenuToggle.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
+
+const MenuToggle = props => {
+    const { onToggle, isOpen } = props;
+    const { MenuToggle, openCls } = classes;
+
+    return (
+         <FontAwesomeIcon
+         className={`${MenuToggle} ${isOpen ? openCls : null}`}
+         icon={isOpen ? faBars : faTimes}
+         onClick={onToggle}
+         size="lg"
+     />
+    )
+
+}
+
+export default MenuToggle;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
 import classes from "./Layout.module.css";
-
+import MenuToggle from "../../components/Navigation/MenuToggle";
 
 
 class Layout extends Component{
+    state = {
+        menu:false
+    }
+  
+    toggleMenuHandler= () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
 
     render() {
         const {children} = this.props;
         const {Layout} = classes;
+
         return (
             <div className={Layout}>
+                <MenuToggle
+                onToggle={this.toggleMenuHandler}
+                isOpen={this.state.menu}
+                />
                 <main>
                     {children}
                 </main>
