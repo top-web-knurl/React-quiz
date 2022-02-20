0fdf89afae141ad4b0ad2464dfb8c0c815940ae8
Timur <58705107+top-web-knurl@users.noreply.github.com>
2022-02-20 11:36:37 +0300

    валидация формы авторизации

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import classes from './Input.module.css';
 
-
 function isInValid({valid, touched, shouldValidate}) {
     return !valid && touched && shouldValidate;
 }
 
 const Input = props => {
     const { value, disabled, type = 'text', legend, onChange, errorMessage} = props;
-    const { Input } = classes;
-
+    const { Input, invalid } = classes;
+    const validFlag = isInValid(props);
     return (
-        <label className={`${Input} ${isInValid(props) ? 'invalid' : null}`}>
+        <label className={`${Input} ${validFlag ? invalid : ''}`}>
             {legend ? <legend>{legend}</legend> : null}
             <input
                 className={Input}
                 disabled={disabled}
                 type={type}
-                value={value}
+                value={value.trim()}
                 onChange={onChange}
             />
 
-            {errorMessage ? <span>{errorMessage}</span> : null}
+            {validFlag ? <span>{errorMessage}</span> : null}
         </label>
     )
 
--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -3,13 +3,110 @@ import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Form from "../../components/UI/Form/Form";
 import Input from "../../components/UI/Input/Input";
+import is from 'is_js';
+
 class Auth extends Component {
+
+  state = {
+    isFormValid: false,
+    formContarols: {
+      email: {
+        value: '',
+        type: 'email',
+        name: 'Ваш email',
+        errorMessage: 'Некорректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        name: 'Пароль',
+        errorMessage: 'Не менее 6 символов',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
 
   loginHandler = () => {
 
   }
   registerHandler = () => {
 
+  }
+
+  validateControl(value, validation) {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (validation.email) {
+      isValid = is.email(value) && isValid;
+    }
+    if (validation.minLength) {
+      isValid = (value.length >= validation.minLength) && isValid;
+    }
+
+    console.log(isValid);
+    return isValid;
+  }
+
+  onChangeHandler = (event, controlName) => {
+
+    const formContarols = { ...this.state.formContarols };
+    const control = { ...formContarols[controlName] }
+
+    control.value = event.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formContarols[controlName] = control;
+
+    let isFormValid = true;
+
+    Object.keys(formContarols).forEach(name => {
+        isFormValid = formContarols[name] && isFormValid;
+    })
+
+    this.setState({
+      isFormValid,
+      formContarols
+    });
+  }
+
+  renderInputs = () => {
+    return Object.keys(this.state.formContarols).map((controlName, index) => {
+      const { type, value, name, valid, touched, errorMessage, validation } = this.state.formContarols[controlName];
+   
+      return (
+        <Input
+          key={index}
+          type={type}
+          value={value}
+          valid={valid}
+          legend={name}
+          touched={touched}
+          errorMessage={errorMessage}
+          shouldValidate={!!validation}
+          onChange={e => this.onChangeHandler(e, controlName)}
+        />
+      )
+
+    })
   }
 
   render() {
@@ -21,29 +118,20 @@ class Auth extends Component {
 
           <Form>
             <fieldset>
-              <Input
-                legend='Ваш email'
-                type="Email"
-              >
-              </Input>
-              <Input
-                legend='Пароль'
-                type="password"
-              >
-              </Input>
-
-
+              {this.renderInputs()}
             </fieldset>
             <fieldset>
               <Button
                 type="success"
                 onClick={this.loginHandler}
+                disabled={this.state.isFormValid}
               >
                 Войти
               </Button>
               <Button
                 type="primary"
                 onClick={this.registerHandler}
+                disabled={this.state.isFormValid}
               >
                 Регистрация
               </Button>
